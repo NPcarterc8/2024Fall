ec7e7925e7b9b333a4d8650d11f8e9964a9436bf
corey carter <coreyc538@gmail.com>
2024-10-28 14:48:04 -0400

    hirew

--- Client/src/components/ShoppingCart.vue
+++ Client/src/components/ShoppingCart.vue
@@ -11,20 +11,43 @@ const cart = refCart()
       <p>Your cart is empty</p>
     </div>
     <div v-else>
-      <div v-for="item in cart" :key="item.product.id">
-        <div>{{ item.product.title }}</div>
-        <div>{{ item.product.price }}</div>
-        <div>{{ item.quantity }}</div>
+      <div v-for="item in cart" :key="item.product.id" class="item">
+        <img :src="item.product.thumbnail" alt="item.product.title" />
+        <button @click="removeFromCart(item.product)" style="float: right">
+          <i class="fas fa-trash is-text-danger"></i>
+        </button>
+        <h3>{{ item.product.title }}</h3>
+        <div>
+          ${{ item.product.price }}*
+          <select v-model="item.quantity">
+            <option v-for="n in 10" :key="n" :value="n">{{ n }}</option>
+          </select>
+          {{ item.quantity }}=${{ (item.product.price * item.quantity).toFixed(2) }}
+        </div>
       </div>
       <div>
-        <p>Total: {{ count }} items = ${{ total }}</p>
+        <h3>{{ count }} items = ${{ total.toFixed(2) }}</h3>
       </div>
     </div>
   </div>
 </template>
 
 <style scoped>
+h3 {
+  font-weight: bold;
+}
 .cart {
   padding: 1rem;
+  overflow-y: auto;
+}
+.cart img {
+  width: 50px;
+  height: 50px;
+  float: left;
+}
+.item {
+  margin-bottom: 0.5rem;
+  padding-bottom: 0.5rem;
+  border-bottom: 1px ridge #ccc;
 }
 </style>
