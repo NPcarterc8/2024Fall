55c7b52821a1cb0f7756fd4d9096b405ecef634e
corey carter <coreyc538@gmail.com>
2024-11-11 14:24:21 -0500

    up to class ?

--- Server/index.js
+++ Server/index.js
@@ -2,12 +2,25 @@
  */
 const express = require("express");
 const app = express();
+const userController = require("./controllers/users");
+const productController = require("./controllers/products");
 
 const PORT = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
+// Middleware
+app.use(express.json());
+app.use(express.static(__dirname + "/dist"));
+
+// Controllers
+app
+  .get("/", (req, res) => {
+    res.send("Hello World");
+  })
+  .get("/about", (req, res) => {
+    res.send("About Us");
+  })
+  .use("/users", userController)
+  .use("/products", productController);
 
 app.listen(PORT, () => {
   console.log("Server is running at http://localhost:" + PORT);
