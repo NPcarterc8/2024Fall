b51d68488b338ec72453d884af6047701940d5b1
corey carter <coreyc538@gmail.com>
2024-11-14 13:45:13 -0500

    up to class ?

--- Server/index.js
+++ Server/index.js
@@ -1,3 +1,5 @@
+/*  B"H
+ */
 const express = require("express");
 const app = express();
 const userController = require("./controllers/users");
@@ -11,15 +13,29 @@ app.use(express.static(__dirname + "/dist"));
 
 // Controllers
 app
-  .get("/", (req, res) => {
+  .get("/", (req, res, next) => {
     res.send("Hello World");
   })
-  .get("/about", (req, res) => {
+  .get("/about", (req, res, next) => {
     res.send("About Us");
   })
-  .use("/users", userController)
-  .use("/products", productController);
+  .use("/api/v1/users", userController)
+  .use("/api/v1/products", productController)
 
-app.listen(PORT, () => {
+  .get("*", (req, res, next) => {
+    res.sendFile(__dirname + "/dist/index.html");
+  });
+
+console.log("Step #1");
+app.listen(PORT, (err, data) => {
+  console.log("Step #2");
   console.log("Server is running at http://localhost:" + PORT);
 });
+console.log("Step #3");
+
+/*  Four types of async methods
+    1. Node Style Callbacks
+    2. Pipelines
+    3. Promises
+    4. Async/Await
+*/
